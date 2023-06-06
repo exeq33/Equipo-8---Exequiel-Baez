be04263a48e981a35e1ca2803744acda55c73a1b
Exe <exequielbaez@gmail.com>
2023-06-06 19:00:58 -0300

    listo

--- CaculadoraDescuento/src/Main.java
+++ CaculadoraDescuento/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double precioOriginal, precioDescuento;
+        double precioOriginal, precioFinal;
         int descuento;
         Scanner entrada = new Scanner(System.in);
 
@@ -12,6 +12,11 @@ public class Main {
 
         System.out.println("Indica el porcentaje de descuento:");
         descuento = entrada.nextInt();
+
+        precioFinal = precioOriginal - (precioOriginal * descuento/100);
+
+        System.out.println("El precio final es: "+precioFinal);
+
 
 
 
